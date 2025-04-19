6bb3d62ba93f4dd31e35bda06fd3b13ae4987402
Akmal.1 <muhak09876@gmail.com>
2025-04-19 22:04:48 +0700

    Update tugas.py
    data update trus

--- tugas.py
+++ tugas.py
@@ -24,5 +24,16 @@ def user_id(user_id):
 def profil(naam):
     return render_template("profil.html", user = naam)
 
+app_name = "My Flask App"  # Variabel global
+
+@app.route('/nama')
+def nama():
+    return f"Welcome to {app_name}!"
+
+@app.route('/data')
+def data():
+    user = {"name": "Ali", "age": 25, "city": "Jakarta"}
+    return render_template('index.html', user=user)
+
 if __name__=="__main__":
     app.run(debug=True)
